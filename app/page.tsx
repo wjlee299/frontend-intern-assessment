480ea1ad8faa83306ec10b542267fd4349dd6847
leewenjin <wenjin299@gmail.com>
2025-09-02 13:19:15 +0800

    Added SVGR dependency for SVG loading, created main UI panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import MainSectionHeader from "./components/sections/MainSectionHeader";
 
 export default function Home() {
   return (
-    <div>New Page</div>
+    <main className="flex h-screen w-screen items-center justify-center">
+      <div className="flex h-[80%] w-[80%] lg:w-[70%] flex-col rounded-lg border border-neutral-n30">
+        <MainSectionHeader></MainSectionHeader>
+      </div>
+    </main>
   );
 }
